e14f291137c9acf8a0722aa13a3385ad46974747
Shaurya Kohli <shauryakohli17@gmail.com>
2023-07-25 16:37:29 +0530

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,12 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   const contactLink = document.getElementById("contactLink");
   const customModal = document.getElementById("customModal");
-
-  // Add event listener to the Contact Us link
-  contactLink.addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent the link from redirecting (optional)
-    displayModal();
-  });
+  let timeoutId;
 
   // Function to display the pop-up (modal)
   function displayModal() {
@@ -18,11 +13,36 @@ document.addEventListener("DOMContentLoaded", function() {
   closeModalBtn.addEventListener("click", function () {
     customModal.style.display = "none";
   });
+
+  // Add event listener to the Contact Us link
+  contactLink.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the link from redirecting (optional)
+    displayModal();
+  });
+
+  // Function to hide the anchor content with a brief delay
+  function hideAnchorContent() {
+    timeoutId = setTimeout(() => {
+      // Set the display property of anchor content to "none"
+      // when the mouse leaves the anchor element
+      this.style.display = "none";
+    }, 600); // Adjust the delay time (in milliseconds) as needed
+  }
+
+  // Get all anchor elements on the entire website
+  const anchorTags = document.querySelectorAll("a");
+
+  // Add event listeners for hover events for each anchor element
+  anchorTags.forEach((anchor) => {
+    anchor.classList.add("hover-underline");
+
+    anchor.addEventListener("mouseenter", function() {
+      clearTimeout(timeoutId);
+      // Set the display property of anchor content to "block"
+      // when the mouse enters the anchor element
+      this.style.display = "block";
+    });
+
+    anchor.addEventListener("mouseleave", hideAnchorContent);
+  });
 });
-
-
-
-
-
-
-
