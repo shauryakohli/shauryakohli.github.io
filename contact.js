17c78f25dee5da25f9fcc659c892fd623643e9a9
Shaurya Kohli <shauryakohli17@gmail.com>
2023-07-28 23:41:25 +0530

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const customModal = document.getElementById("customModal");
   const contactLink = document.getElementById("contactLink");
-  let timer;
+  const customModal = document.getElementById("customModal");
+  const closeModalBtn = document.getElementById("closeModal");
 
-  // Function to display the pop-up (modal)
+  // Function to show the pop-up (modal)
   function displayModal() {
     customModal.style.display = "block";
-    clearTimeout(timer); // Clear the previous timer (if any)
   }
 
   // Function to hide the pop-up (modal)
@@ -14,25 +13,24 @@ document.addEventListener("DOMContentLoaded", function() {
     customModal.style.display = "none";
   }
 
-  // Add mouseenter and mouseleave event listeners
-  contactLink.addEventListener("mouseenter", function () {
-    // Show the modal when the user hovers over the link
+  // Add event listener to the Contact Us link to show the pop-up on hover
+  contactLink.addEventListener("mouseenter", function() {
     displayModal();
   });
 
-  contactLink.addEventListener("mouseleave", function () {
-    // Hide the modal when the user moves the mouse away from the link
-    // Set a timer to hide the modal after 1 second (1000 milliseconds)
-    timer = setTimeout(hideModal, 3500);
+  // Add event listener to the X button to hide the pop-up
+  closeModalBtn.addEventListener("click", function() {
+    hideModal();
   });
 
-  // Close the pop-up when clicking the close button (x)
-  const closeModalBtn = document.getElementById("closeModal");
-  closeModalBtn.addEventListener("click", hideModal);
-
-  // Close the pop-up when clicking outside the modal content
-  window.addEventListener("click", function (event) {
-    if (event.target === customModal) {
+  // Close the pop-up when the mouse leaves the link or the modal
+  document.addEventListener("mouseleave", function(event) {
+    if (
+      event.target !== contactLink &&
+      event.target !== customModal &&
+      !contactLink.contains(event.target) &&
+      !customModal.contains(event.target)
+    ) {
       hideModal();
     }
   });
