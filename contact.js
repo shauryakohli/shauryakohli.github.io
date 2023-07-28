ed26f28262ebb135404374547d2dc5a10300e9cf
Shaurya Kohli <shauryakohli17@gmail.com>
2023-07-29 02:45:42 +0530

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,30 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const contactLink = document.getElementById("contactLink");
-  const customModal = document.getElementById("customModal");
+const contactLink = document.getElementById("contactLink");
+const customModal = document.getElementById("customModal");
+const closeBtn = document.getElementById("closeModal");
+let hoverTimeoutId;
 
-  // Function to show the pop-up (modal)
-  function displayModal() {
-    customModal.style.display = "block";
-  }
-
-  // Close the pop-up when clicking the close button (x)
-  const closeModalBtn = document.getElementById("closeModal");
-  closeModalBtn.addEventListener("click", function() {
-    customModal.style.display = "none";
-  });
-
-  // Close the pop-up when clicking outside the modal content
-  window.addEventListener("click", function(event) {
-    if (event.target === customModal) {
-      customModal.style.display = "none";
-    }
-  });
-
-  // Show the pop-up on hover
-  contactLink.addEventListener("mouseover", displayModal);
-
-  // Hide the pop-up when mouse is not hovering over the link
-  contactLink.addEventListener("mouseout", function() {
-    customModal.style.display = "none";
-  });
+// Function to show the contact box on hover
+contactLink.addEventListener("mouseenter", function () {
+  customModal.style.display = "block";
 });
+
+// Function to hide the contact box when the mouse leaves the link
+contactLink.addEventListener("mouseleave", function () {
+  clearTimeout(hoverTimeoutId);
+  hoverTimeoutId = setTimeout(() => {
+    customModal.style.display = "none";
+  }, 20000); // Adjust the delay time (in milliseconds) as needed
+});
+
+// Function to close the contact box when clicking the close button (x)
+closeBtn.addEventListener("click", function () {
+  customModal.style.display = "none";
+});
+
+// Function to close the contact box when clicking outside the modal content
+window.addEventListener("click", function (event) {
+  if (event.target === customModal) {
+    customModal.style.display = "none";
+  }
+});
+
+
+
+
+
+
