0df697a7f3235961e3495ffbd5a2b34757ff6606
Shaurya Kohli <shauryakohli17@gmail.com>
2023-07-29 00:20:27 +0530

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,37 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const contactLink = document.getElementById("contactLink");
   const customModal = document.getElementById("customModal");
-  const closeModalBtn = document.getElementById("closeModal");
 
-  // Function to show the pop-up (modal)
+  // Add event listener to the Contact Us link
+  contactLink.addEventListener("mouseover", function () {
+    displayModal();
+  });
+
+  // Function to display the pop-up (modal)
   function displayModal() {
     customModal.style.display = "block";
   }
 
-  // Function to hide the pop-up (modal)
-  function hideModal() {
+  // Close the pop-up when clicking the close button (x)
+  const closeModalBtn = document.getElementById("closeModal");
+  closeModalBtn.addEventListener("click", function () {
     customModal.style.display = "none";
-  }
-
-  // Add event listener to the Contact Us link to show the pop-up on hover
-  contactLink.addEventListener("mouseenter", function() {
-    displayModal();
   });
 
-  // Add event listener to the X button to hide the pop-up
-  closeModalBtn.addEventListener("click", function() {
-    hideModal();
-  });
-
-  // Close the pop-up when the mouse leaves the link or the modal
-  document.addEventListener("mouseleave", function(event) {
-    if (
-      event.target !== contactLink &&
-      event.target !== customModal &&
-      !contactLink.contains(event.target) &&
-      !customModal.contains(event.target)
-    ) {
-      hideModal();
+  // Close the pop-up when clicking outside the modal content
+  window.addEventListener("click", function (event) {
+    if (event.target === customModal) {
+      customModal.style.display = "none";
     }
   });
 });
